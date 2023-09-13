5f9128fbed55d90b868720a4375f4a2dd91352df
Imanol Echazarreta <kitesalett@gmail.com>
2023-09-13 09:44:20 -0300

    Encriptacion del password seedeado de usuarios

--- IntegradorSofttekImanol/DAL/DBSeeding/UsuarioSeeder.cs
+++ IntegradorSofttekImanol/DAL/DBSeeding/UsuarioSeeder.cs
@@ -1,4 +1,5 @@
-﻿using IntegradorSofttekImanol.Models.Entities;
+﻿using IntegradorSofttekImanol.Helpers;
+using IntegradorSofttekImanol.Models.Entities;
 using IntegradorSofttekImanol.Models.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,7 +19,7 @@ namespace IntegradorSofttekImanol.DAL.DBSeeding
                 new Usuario
                 {
                     CodUsuario = 1,
-                    Contrasena = "xxxxxxxx",
+                    Contrasena = EncrypterHelper.Encrypter("xxxdsaddsds"),
                     Dni = 39382743,
                     Nombre = "random",
                     FechaAlta = DateTime.Now
