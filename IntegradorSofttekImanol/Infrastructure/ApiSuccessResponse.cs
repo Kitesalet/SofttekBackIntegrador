800b2bb393a1300173072fbbf39a487ddb1e2c37
Imanol Echazarreta <kitesalett@gmail.com>
2023-09-16 00:24:42 -0300

    Cambios en XCXXX

---
+++ IntegradorSofttekImanol/Infrastructure/ApiSuccessResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace IntegradorSofttekImanol.Infrastructure
+{
+    public class ApiSuccessResponse
+    {
+
+        public int StatusCode { get; set; }
+
+        public object Data { get; set; }
+    }
+}
