ee4f646ddc57bd758f0a0ab62ab210f98162b199
Imanol Echazarreta <kitesalett@gmail.com>
2023-09-17 17:17:12 -0300

    Documentación de los endpoints de Usuarios

--- IntegradorSofttekImanol/Controllers/UsuarioController.cs
+++ IntegradorSofttekImanol/Controllers/UsuarioController.cs
@@ -12,12 +12,15 @@ namespace IntegradorSofttekImanol.Controllers
     public class UsuarioController : ControllerBase
     {
         private readonly IUsuarioService _service;
+
         public UsuarioController(IUsuarioService service)
         {
-            _service = service; 
+            _service = service;
         }
 
-
+        /// <summary>
+        /// Obtiene todos los usuarios.
+        /// </summary>
         [HttpGet]
         [Route("usuarios")]
         public async Task<ActionResult<IEnumerable<UsuarioGetDto>>> GetAllUsuarios()
@@ -27,76 +30,87 @@ namespace IntegradorSofttekImanol.Controllers
             return Ok(users);
         }
 
+        /// <summary>
+        /// Obtiene un usuario por su id.
+        /// </summary>
+        /// <param name="id">id del usuario a buscar.</param>
         [HttpGet]
         [Route("usuario/{id}")]
         public async Task<ActionResult<UsuarioLoginDto>> GetUsuario([FromRoute] int id)
         {
-           var user = await _service.GetUsuarioByIdAsync(id);
+            var user = await _service.GetUsuarioByIdAsync(id);
 
-           if(user == null)
-           {
-                return NotFound("No se ha encontrado el usuario!");
-           }
+            if (user == null)
+            {
+                return NotFound("No se ha encontrado el usuario.");
+            }
 
-           return Ok(await _service.GetUsuarioByIdAsync(id));
-            
+            return Ok(await _service.GetUsuarioByIdAsync(id));
         }
 
+        /// <summary>
+        /// Crea un nuevo usuario.
+        /// </summary>
+        /// <param name="usuario">Datos del usuario a crear en un dto.</param>
         [HttpPost]
         [Route("register")]
         public async Task<ActionResult> CreateUsuario(UsuarioCreateDto usuario)
         {
-
             var flag = await _service.CreateUsuarioAsync(usuario);
 
-            if(flag == false)
+            if (!flag)
             {
                 return BadRequest(flag);
             }
 
-            return CreatedAtRoute("",flag);
-
+            return CreatedAtRoute("", flag);
         }
 
+        /// <summary>
+        /// Actualiza un usuario existente por su id.
+        /// </summary>
+        /// <param name="id">id del usuario a actualizar.</param>
+        /// <param name="usuario">Datos actualizados del usuario en un dto</param>
         [HttpPut]
         [Route("usuario/{id}")]
         public async Task<ActionResult> UpdateUsuario(int id, UsuarioUpdateDto usuario)
-        {   
-
-            if(await _service.GetUsuarioByIdAsync(id) == null)
+        {
+            if (await _service.GetUsuarioByIdAsync(id) == null)
             {
-                return NotFound("El usuario no ha sido encontrado!");
+                return NotFound("El usuario no ha sido encontrado.");
             }
 
             var result = await _service.UpdateUsuario(usuario);
 
-            if(result != true)
+            if (!result)
             {
-                return BadRequest("Ha habido un error en el update del usuario");
+                return BadRequest("Ha habido un error en la actualización del usuario.");
             }
 
             return NoContent();
         }
 
+        /// <summary>
+        /// Elimina un usuario por su id.
+        /// </summary>
+        /// <param name="id">id del usuario a eliminar.</param>
         [HttpDelete]
         [Route("usuario/{id}")]
         public async Task<ActionResult> DeleteUsuario([FromRoute] int id)
         {
-
             if (await _service.GetUsuarioByIdAsync(id) == null)
             {
-                return NotFound("El usuario no ha sido encontrado!");
+                return NotFound("El usuario no ha sido encontrado.");
             }
 
             var result = await _service.DeleteUsuarioAsync(id);
 
-            if(result != true)
+            if (!result)
             {
                 return BadRequest(result);
             }
 
             return NoContent();
         }
-
     }
 }
