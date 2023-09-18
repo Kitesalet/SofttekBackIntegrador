5ee8f750729590fac7d2932296fef2e95f697da5
Imanol Echazarreta <kitesalett@gmail.com>
2023-09-18 00:16:39 -0300

    Se documento la clase UnitOfWork and its interface

--- IntegradorSofttekImanol/Services/UnitOfWork.cs
+++ IntegradorSofttekImanol/Services/UnitOfWork.cs
@@ -4,12 +4,18 @@ using IntegradorSofttekImanol.Models.Interfaces;
 
 namespace IntegradorSofttekImanol.Services
 {
+    /// <summary>
+    /// The implemmentation of a unit that manages repositories and databases.
+    /// </summary>
     public class UnitOfWork : IUnitOfWork
     {
-
         private readonly AppDbContext _context;
-        public UsuarioRepository UsuarioRepository { get ; private set; }
+        public UsuarioRepository UsuarioRepository { get; }
 
+        /// <summary>
+        /// Initializes an instance of UnitOfWork using dependency injection with its parameters
+        /// </summary>
+        /// <param name="context">AppDbContext with DI</param>
         public UnitOfWork(AppDbContext context)
         {
             _context = context;
@@ -18,14 +24,7 @@ namespace IntegradorSofttekImanol.Services
 
         public async Task<int> Complete()
         {
-
             return await _context.SaveChangesAsync();
-           
-        }
-
-        public void Dispose()
-        {
-            _context.Dispose();
         }
     }
 }
