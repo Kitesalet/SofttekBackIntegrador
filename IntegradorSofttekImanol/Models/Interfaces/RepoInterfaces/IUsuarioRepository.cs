f223a9c6843f2f9f4eec9ed894959e22458542cd
Imanol Echazarreta <kitesalett@gmail.com>
2023-09-18 00:25:44 -0300

    Documenté UsuarioService y la interfaz IUsuarioService

--- IntegradorSofttekImanol/Models/Interfaces/RepoInterfaces/IUsuarioRepository.cs
+++ IntegradorSofttekImanol/Models/Interfaces/RepoInterfaces/IUsuarioRepository.cs
@@ -2,6 +2,9 @@
 
 namespace IntegradorSofttekImanol.Models.Interfaces.RepoInterfaces
 {
+    /// <summary>
+    /// This interface defines extra repository operations related to the Usuario entity
+    /// </summary>
     public interface IUsuarioRepository : IRepository<Usuario>
     {
 
