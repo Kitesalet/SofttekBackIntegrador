d84ccd963c7e3abddc8f5ea85a1eee8e6c10b913
Imanol Echazarreta <kitesalett@gmail.com>
2023-09-18 01:18:34 -0300

    Docummentes every entity in the database

--- IntegradorSofttekImanol/Models/Entities/BaseEntity.cs
+++ IntegradorSofttekImanol/Models/Entities/BaseEntity.cs
@@ -1,17 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IntegradorSofttekImanol.Models.Entities
 {
+    /// <summary>
+    /// Represents a base entity.
+    /// </summary>
     public class BaseEntity
     {
-        [Column("fechaBaja")]
-        public DateTime? FechaBaja { get; set; }
 
-        [Column("fechaAlta")]
+        [Column("fechaBaja", TypeName = "datetime")]
+        public DateTime FechaBaja { get; set; }
+
+        [Column("fechaAlta", TypeName = "datetime")]
+        [Required]
         public DateTime FechaAlta { get; set; }
 
-        [Column("fechaUpdate")]
-        public DateTime? FechaUpdate { get; set; }
-
+        [Column("fechaUpdate", TypeName = "datetime")]
+        public DateTime FechaUpdate { get; set; }
     }
 }
--- IntegradorSofttekImanol/Models/Entities/Rol.cs
+++ IntegradorSofttekImanol/Models/Entities/Rol.cs
@@ -4,22 +4,25 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace IntegradorSofttekImanol.Models.Entities
 {
     [Table("roles")]
+
+    /// <summary>
+    /// Represents a role entity.
+    /// </summary>
     public class Rol : BaseEntity
     {
         [Key]
-        [Column("codRol")]
-        public int codRol { get; set; }
+        [Column("codRol", TypeName = "int")]
+        public int CodRol { get; set; }
 
-        [Column("nombre")]
+        [Column("nombre", TypeName = "varchar(45)")]
+        [Required]
         public string Nombre { get; set; }
 
-        [Column("description")]
+        [Column("descripcion", TypeName = "varchar(200)")]
+        [Required]
         public string Descripcion { get; set; }
 
-        
-        //Navigation Properties
-
+        // Navigation Properties
         public IEnumerable<Usuario> Usuarios { get; set; }
-
     }
 }
