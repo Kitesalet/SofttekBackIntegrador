2c16e6003bd2d0d0a4f80bf49f7403fe1ac653fe
Imanol Echazarreta <kitesalett@gmail.com>
2023-09-17 23:49:38 -0300

    Se documento IRepository

--- IntegradorSofttekImanol/Models/Interfaces/RepoInterfaces/IRepository.cs
+++ IntegradorSofttekImanol/Models/Interfaces/RepoInterfaces/IRepository.cs
@@ -3,12 +3,46 @@ using System.Collections.Generic;
 
 namespace IntegradorSofttekImanol.Models.Interfaces.RepoInterfaces
 {
+    /// <summary>
+    /// This interface defines common operations for a repository handling entities of type T.
+    /// </summary>
+
     public interface IRepository<T> where T : class
     {
-        Task<IEnumerable<T>> GetAll();
-        Task<T> GetById(int id);
-        public Task AddAsync(T entity);
-        public bool Delete(int id);
-        public void Update(T entity);
+        /// <summary>
+        /// Gets all entities of type T.
+        /// </summary>
+        /// <returns>An asynchronous operation that returns a collection of entities.</returns>
+        Task<IEnumerable<T>> GetAllAsync();
+
+        /// <summary>
+        /// Gets an entity of type T by its id.
+        /// </summary>
+        /// <param name="id">An int</param>
+        /// <returns>All of the entities</returns>
+        Task<T> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Adds an entity of type T.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        /// <returns>An entity by its ID</returns>
+        Task AddAsync(T entity);
+
+        /// <summary>
+        /// Deletes an entity of type T by its id.
+        /// </summary>
+        /// <param name="id">An int</param>
+        /// <returns>
+        /// *True if it was deleted
+        /// *False if it wasnt deleted
+        /// </returns>
+        bool Delete(int id);
+
+        /// <summary>
+        /// Updates an entity of type T.
+        /// </summary>
+        /// <param name="entity">The entity to update</param>
+        void Update(T entity);
     }
 }
