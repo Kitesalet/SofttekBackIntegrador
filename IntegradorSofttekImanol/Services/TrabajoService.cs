3d1d6fe731f8835643059406ea3d60747f3296b4
Imanol Echazarreta <kitesalett@gmail.com>
2023-09-20 00:23:02 -0300

    Se finalizo de configurar el TrabajoService.cs

--- IntegradorSofttekImanol/Services/TrabajoService.cs
+++ IntegradorSofttekImanol/Services/TrabajoService.cs
@@ -1,35 +1,116 @@
-﻿using IntegradorSofttekImanol.Models.DTOs;
+﻿using AutoMapper;
+using IntegradorSofttekImanol.Models.DTOs;
+using IntegradorSofttekImanol.Models.DTOs.Servicio;
 using IntegradorSofttekImanol.Models.DTOs.Trabajo;
 using IntegradorSofttekImanol.Models.DTOs.Usuario;
+using IntegradorSofttekImanol.Models.Entities;
+using IntegradorSofttekImanol.Models.Interfaces;
 using IntegradorSofttekImanol.Models.Interfaces.ServiceInterfaces;
 
 namespace IntegradorSofttekImanol.Services
 {
+
+    /// <summary>
+    /// The implementation of the service for defining and using UsuarioDtos and its logic.
+    /// </summary>
     public class TrabajoService : ITrabajoService
     {
-        public Task<bool> CreateTrabajoAsync(TrabajoCreateDto trabajoDto)
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        /// <summary>
+        /// Initializes an instance of ServicioService using dependency injection with its parameters
+        /// </summary>
+        /// <param name="unitOfWork">IUnitOfWork with DI</param>
+        /// <param name="mapper">IMapper with DI</param>
+        public TrabajoService(IUnitOfWork unitOfWork, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
 
-        public Task<bool> DeleteTrabajoAsync(int id)
+        /// <inheritdoc/>
+        public async Task<bool> CreateTrabajoAsync(TrabajoCreateDto trabajoDto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var trabajo = _mapper.Map<Trabajo>(trabajoDto);
+
+                await _unitOfWork.TrabajoRepository.AddAsync(trabajo);
+
+                trabajo.valorHora = trabajoDto.valorHora;
+                trabajo.CantHoras = trabajoDto.CantHoras;
+                trabajo.CodServicio = trabajoDto.CodServicio;
+                trabajo.CodProyecto = trabajoDto.CodProyecto;
+                trabajo.Fecha = trabajoDto.Fecha;
+                trabajo.FechaAlta = DateTime.Now;
+
+                await _unitOfWork.Complete();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + " - Error");
+                return false;
+            }
         }
 
-        public Task<IEnumerable<TrabajoGetDto>> GetAllTrabajosAsync(int page, int units)
+        /// <inheritdoc/>
+        public async Task<bool> DeleteTrabajoAsync(int id)
         {
-            throw new NotImplementedException();
+            var flag = await _unitOfWork.TrabajoRepository.Delete(id);
+
+            await _unitOfWork.Complete();
+
+            return flag;
         }
 
-        public Task<TrabajoGetDto> GetTrabajoByIdAsync(int id)
+        /// <inheritdoc/>
+        public async Task<IEnumerable<TrabajoGetDto>> GetAllTrabajosAsync(int page, int units)
         {
-            throw new NotImplementedException();
+            var trabajos = await _unitOfWork.TrabajoRepository.GetAllAsync(page, units, e => e.Proyecto, e => e.Servicio);
+
+            var trabajosDto = _mapper.Map<List<TrabajoGetDto>>(trabajos);
+
+            return trabajosDto;
         }
 
-        public Task<bool> UpdateTrabajo(TrabajoUpdateDto trabajoDto)
+        /// <inheritdoc/>
+        public async Task<TrabajoGetDto> GetTrabajoByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var trabajo = await _unitOfWork.TrabajoRepository.GetByIdAsync(id);
+
+            if (trabajo == null || trabajo.FechaBaja != null)
+            {
+                return null;
+            }
+
+            return _mapper.Map<TrabajoGetDto>(trabajo);
+        }
+
+        /// <inheritdoc/>
+        public async Task<bool> UpdateTrabajo(TrabajoUpdateDto trabajoDto)
+        {
+            var trabajo = await _unitOfWork.TrabajoRepository.GetByIdAsync(trabajoDto.codTrabajo);
+
+            if (trabajo == null)
+            {
+                return false;
+            }
+
+            trabajo.CodProyecto = trabajoDto.CodProyecto;
+            trabajo.CodServicio = trabajoDto.CodServicio;
+            trabajo.CantHoras = trabajoDto.CantHoras;
+            trabajo.valorHora = trabajoDto.valorHora;
+            trabajo.FechaUpdate = DateTime.Now;
+
+            _unitOfWork.TrabajoRepository.Update(trabajo);
+
+            await _unitOfWork.Complete();
+
+            return true;
         }
     }
 }
